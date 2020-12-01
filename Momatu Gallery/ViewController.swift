b746f9d626844e423cfc9d0749c696652293e6d0
Lexon <lexonli@hotmail.com>
2020-12-01 18:13:45 +1100

    Initial Commit

---
+++ Momatu Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Momatu Gallery
+//
+//  Created by Lexon on 1/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Momatu GalleryTests/Momatu_GalleryTests.swift
@@ -0,0 +1,33 @@
+//
+//  Momatu_GalleryTests.swift
+//  Momatu GalleryTests
+//
+//  Created by Lexon on 1/12/2020.
+//
+
+import XCTest
+@testable import Momatu_Gallery
+
+class Momatu_GalleryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
